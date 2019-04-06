3b483aaf0a42b429a810a5b6ffc137afe592bc3c
Jang Wonbeom <jtiger958@gmail.com>
2019-04-06 17:14:10 +0900

    edit gitignore, make model, make dataset

---
+++ dataloader/dataset.py
@@ -0,0 +1,42 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+import glob
+from torchvision import transforms
+
+class GANDataset(Dataset):
+    def __init__(self, data_folder, from_style, to_style, image_size=224):
+        self.data_folder = data_folder
+        self.image_size = image_size
+
+        if not os.path.exists(data_folder):
+            raise Exception(f"[!] {self.data_folder} not exited")
+
+        self.files_A = sorted(glob.glob(os.path.join(data_folder, f"{from_style}", "*.*")))
+        self.files_B = sorted(glob.glob(os.path.join(data_folder, f"{to_style}", "*.*")))
+
+    def __getitem__(self, item, transform=False):
+        image_A = self.files_A[item]
+        image_B = self.files_B[item]
+
+        image_A = Image.open(image_A).convert('RGB')
+        image_B = Image.open(image_B).convert('RGB')
+
+        if transform:
+            transform_A = transforms.Compose([
+                transforms.CenterCrop(min(image_A.size[0], image_A.size[1])),
+                transforms.Resize(self.image_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ])
+            transform_B = transforms.Compose([
+                transforms.CenterCrop(min(image_B.size[0], image_B.size[1])),
+                transforms.Resize(self.image_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ])
+
+            image_A = transform_A(image_A)
+            image_B = transform_B(image_B)
+
+        return {'A':image_A, 'B':image_B}   
---
+++ model/model.py
@@ -0,0 +1,78 @@
+import torch.nn as nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channel):
+        super(ResidualBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channel, out_channels=in_channel, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(in_channel),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        out = self.block(x)
+        return out + x
+
+
+class ResidualGenerator(nn.Module):
+    def __init__(self, in_shape, num_residual_block):
+        super(ResidualGenerator, self).__init__()
+        in_channels = in_shape[0]
+        out_channels = 64
+
+        self.model = [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=7),
+                      nn.InstanceNorm2d(out_channels),
+                      nn.ReLU(inplace=True)]
+        in_channels = out_channels
+
+        for _ in range(2):
+            out_channels *= 2
+            self.model += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2),
+                           nn.InstanceNorm2d(out_channels),
+                           nn.ReLU(inplace=True)]
+            in_channels = out_channels
+
+        for _ in range(num_residual_block):
+            self.model += [ResidualBlock(in_channels)]
+
+        for _ in range(2):
+            out_channels //= 2
+            self.model += [nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2),
+                           nn.InstanceNorm2d(out_channels),
+                           nn.ReLU(inplace=True)]
+            in_channels = out_channels
+
+        self.model = [nn.Conv2d(in_channels=in_channels, out_channels=3, kernel_size=7),
+                      nn.InstanceNorm2d(out_channels),
+                      nn.ReLU(inplace=True)]
+
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_shape):
+        super(Discriminator, self).__init__()
+        in_channels = in_shape[0]
+        out_channels=64
+
+        self.model = []
+        for _ in range(4):
+            self.model += self.discriminator_block(in_channels=in_channels, out_channels=out_channels)
+            in_channels = out_channels
+            out_channels *= 2
+        in_channels = out_channels
+
+        self.model += [nn.Conv2d(in_channels=in_channels, out_channels=1, kernel_size=4),
+                       nn.Sigmoid()]
+
+    def discriminator_block(self, in_channels, out_channels, nomalization=True):
+        block = [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=4, stride=2),
+                 nn.InstanceNorm2d(out_channels),
+                 nn.LeakyReLU(inplace=True)]
+
+        return block
