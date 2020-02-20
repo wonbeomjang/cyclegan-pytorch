a5690ecd9df137a744f0e64e35c043723c4566ef
wonbeomjang <jtiger958@gmail.com>
2020-02-20 20:44:05 +0900

    correct download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -8,8 +8,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--style', type=str, default='monet2photo', help='the path of image data set')
 args = parser.parse_args()
 
-monet2photo_url = 'https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/monet2photo.zip'
-apple2orange_url = 'https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/apple2orange.zip'
+urls = {'monet2photo': 'https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/monet2photo.zip',
+        'apple2orange': 'https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/apple2orange.zip'}
 
 from_style = args.style.split('2')[0]
 to_style = args.style.split('2')[1]
@@ -24,7 +24,7 @@ if not os.path.exists(os.path.join('dataset', to_style)):
     os.makedirs(os.path.join('dataset', to_style))
 
 
-download_url(monet2photo_url, os.path.join('temp', f'{args.style}.zip'))
+download_url(urls[args.style], os.path.join('temp', f'{args.style}.zip'))
 unzip_zip_file(os.path.join('temp', f'{args.style}.zip'), 'temp')
 
 images_A = os.listdir(os.path.join('temp', args.style, 'trainA'))
